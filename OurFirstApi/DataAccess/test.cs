1c8c9e9b4107208cbb3757db07d5c5bdad860d73
Nathan Gonzalez <nathan@copypastedeveloper.com>
2017-09-09 09:35:19 -0500

    more new file things

---
+++ OurFirstApi/DataAccess/test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OurFirstApi.DataAccess
+{
+    public class Test2
+    {
+        public Test2()
+        {
+            Console.WriteLine("Doing things");
+        }
+    }
+}
